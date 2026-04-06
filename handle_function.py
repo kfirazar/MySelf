bfa3f5344f5304465cf1f66464792c01ca2a8001
kfir <kfirshimonov2@gmail.com>
2026-04-06 23:32:22 +0300

    Mini refactor of base code for general purpose for future development

--- handle_function.py
+++ handle_function.py
@@ -2,7 +2,6 @@ import json
 import inner_function
 
 
-
 # This function creates a JSON file with the specified filename and initializes it with an empty dictionary.
 def create_json_file(filename='group.json'):
     data = {
@@ -17,20 +16,57 @@ def create_json_file(filename='group.json'):
 # The function can also accept additional parameters, which can be used to customize the group object further.
 # For time complexity, the ids will be keys in the JSON file, so we can directly access the group object using its unique ID without needing to loop through the entire file. This allows for efficient retrieval and updating of group objects based on their unique IDs.
 def create_group_object(group_name, **kwargs):
-    group_id = inner_function.generate_random_id()
+    group_id = inner_function.generate_virtual_id(group_name)
     group_object = {
         "id": group_id,
         "name": group_name,
+        "type": "group",
+        "tasks": [],
         # Add any additional parameters to the group object
         **kwargs
     }
     return group_object
 
+# This function creates a task and adds it to the specified group object.
+def create_task_object(group_id, task_name, filename='group.json', **kwargs):
+    task_id = inner_function.generate_random_id()
+    task_object = {
+        "id": task_id,
+        "name": task_name,
+        "group_id": group_id,
+        "type": "task",
+        **kwargs
+    }
+    
+    # Load data
+    try:
+        with open(filename, 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        data = {}
+    
+    # Add task to data
+    data[task_id] = task_object
+    
+    # Add task_id to group's tasks list
+    if group_id in data and data[group_id].get("type") == "group":
+        data[group_id]["tasks"].append(task_id)
+    
+    # Save data
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    
+    return task_object
+
+
 # This function retrieves the group object from the JSON file based on the provided group name.
 # Can return several group objects if there are multiple groups with the same name.
-def get_group_object(group_name):
-    with open('group.json', 'r') as json_file:
-        data = json.load(json_file)
+def get_group_object(group_name, filename='group.json'):
+    try:
+        with open(filename, 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        return []
     
     # Filter the group objects based on the provided group name
     group_objects = [group for group in data.values() if group.get("name") == group_name and group.get("type") == "group"]
@@ -39,38 +75,48 @@ def get_group_object(group_name):
 
 
 
+# This function removes specific fields from the given object dictionary
+# The fields to remove are given by *args
+def remove_fields_from_object(obj, *args):
+    for field in args:
+        if field in obj:
+            del obj[field]
+    return obj
 
-"""
- The data is saving/updating in a structured format, making it easy to read and access later.
- Currently, the type saved in _types object.
- If group object DOES NOT exist in the JSON file, it will be added to the file.
- If the group object already exists (based on the group id), it will be updated with the new information provided in the group_object parameter.
- The KEY value for a group object in the JSON file is the unique ID of the group, which is generated using the generate_random_id function.
-"""
-def save_to_json_file(group_object, filename='group.json'):
-    with open(filename, 'r') as json_file:
-        data = json.load(json_file) 
+
+# This next function meant to be a base function function for saving an object in Json file
+# All valadiation will occurs ahead of the function in other logic secton
+# The "id" of the wanted ovbject will be given as a parameter
+def save_object_to_json_file(object_to_save, object_id, filename):
+    try:
+        with open(filename, 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        data = {}
     
-    # Get the group ID
-    group_id = group_object.get("id")
-    group_object.pop("id")  
-
-    # Check if the group object already exists in the JSON file by checking if the ID exists as a key (O(1) operation)
-    if group_id in data:
-        # Update the existing group object with the new information
-        data[group_id].update(group_object)
+    # Check if the object already exists in the JSON file by checking if the ID exists as a key (O(1) operation)
+    if object_id in data:
+        # Update the existing object with the new information
+        data[object_id].update(object_to_save)
         
     else:
-        # Add the new group object to the JSON file using its unique ID as the key
-        data[group_id] = group_object
+        # Add the new object to the JSON file using its unique ID as the key
+        data[object_id] = object_to_save
 
     # Write the updated data back to the JSON file
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=4)
-    
+
+
+
     
 
 g1 = create_group_object("Group 1", description="This is the first group",category="A")
-save_to_json_file(g1)
+g1_id = g1["id"]
+g1 = remove_fields_from_object(g1, "id")
+save_object_to_json_file(g1, g1_id, 'group.json')
+
 g2 = create_group_object("Group 2", description="This is the second group")
-save_to_json_file(g2)
+g2_id = g2["id"]
+g2 = remove_fields_from_object(g2, "id")
+save_object_to_json_file(g2, g2_id, 'group.json')
--- inner_function.py
+++ inner_function.py
@@ -1,3 +1,4 @@
+import json
 import random
 import uuid
 
@@ -12,3 +13,23 @@ _types ={
 #Which is unique and not used before, you can implement this function to generate a random ID
 def generate_random_id():
     return str(uuid.uuid4())
+
+
+
+# This function get a common group name and generates a unique virtual ID.
+# This Virtual ID is used for id for group objects.
+# The mapping is getting saved in the virtual_address.json file, which is a dictionary that maps the common group name to the generated virtual ID.
+def generate_virtual_id(group_name):
+    with open('virtual_address.json', 'r') as json_file:
+        virtual_address = json.load(json_file)
+    
+    if group_name in virtual_address:
+        return virtual_address[group_name]
+    else:
+        virtual_id = generate_random_id()
+        virtual_address[group_name] = virtual_id
+        
+        with open('virtual_address.json', 'w') as json_file:
+            json.dump(virtual_address, json_file, indent=4)
+        
+        return virtual_id
